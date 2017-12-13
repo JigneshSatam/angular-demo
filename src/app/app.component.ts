8c4f9d34ac014294ae4ffd6872d109884953292b
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-12-14 00:57:19 +0530

    Angular demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  // title = 'app';
+  // obj = {
+  //   id: "1",
+  //   name: "Test"
+  // };
+  // arr = ["abc", 'def', 'ghi']
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { MembersComponent } from './members/members.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    MembersComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: ProductsComponent
+
+      },
+      {
+        path: 'members',
+        component: MembersComponent
+
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
