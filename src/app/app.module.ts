b3b269912b3f8a5568e1a20dfeb169115db73bbf
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2018-01-05 01:49:31 +0530

    Added animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 
 
@@ -18,7 +19,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuardService } from "./auth-guard.service";
 import { AuthService } from './auth.service';
 import { LoginModule } from './login/login.module';
-import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { CoreModule } from './core/core.module'
 
 @NgModule({
@@ -30,10 +30,10 @@ import { CoreModule } from './core/core.module'
     HeroDetailComponent,
     // MessagesComponent,
     // DashboardComponent,
-    CrisisCenterComponent,
     // CoreComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     LoginModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { trigger, transition, style, animate, state, keyframes } from "@angular/animations";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger("someCoolAnimation", [
+      state("fadeIn", style({ opacity: 1, transform: 'scale(1.0)'})),
+      state("fadeOut", style({ opacity: 0, transform: 'scale(0.0)'})),
+      transition("* => fadeIn", [
+        // fade in Animation
+        animate(1000)
+      ]),
+      transition("* => fadeOut", [
+        // fade out Animation
+        // ---- 1 ----
+        // animate(1000)
+
+        // ---- 2 ----
+        // animate('2000ms ease-in', keyframes([
+        //   style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        //   style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
+        //   style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+        // ]))
+
+        // ---- 3 ----
+        animate(2000, keyframes([
+          style({opacity: 1, offset: 0}),
+          style({opacity: 0, offset: 0.20}),
+          style({opacity: 1, offset: 0.40}),
+          style({opacity: 0, offset: 0.60}),
+          style({opacity: 1, offset: 0.80}),
+          style({opacity: 0, transform: 'scale(0.0)', offset: 1.0}),
+        ]))
+      ])
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
@@ -23,6 +56,7 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.fadeOut();
     this.message = 'Trying to log in ...';
 
     this.authService.login().subscribe(() => {
@@ -36,10 +70,25 @@ export class LoginComponent implements OnInit {
         this.router.navigate([redirect]);
       }
     });
+    // this.fadeIn();
   }
 
   logout() {
     this.authService.logout();
     this.setMessage();
+  }
+
+  bindingVar = '';
+
+  fadeIn(){
+    this.bindingVar = 'fadeIn';
+  }
+
+  fadeOut(){
+    this.bindingVar = 'fadeOut';
+  }
+
+  toggle() {
+    this.bindingVar == 'fadeOut' ? this.fadeIn() : this.fadeOut();
   }
 }
